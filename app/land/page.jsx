df0b967861e91f6820d43e9bb5c39f8f4d3b0745
Taldarion <fasch00@icloud.com>
2025-06-22 13:47:59 +0200

    tried fix for database connection issue

--- app/land/page.jsx
+++ app/land/page.jsx
@@ -13,11 +13,7 @@ export default async function Page() {
             <h1 className="mb-8">Land</h1>
             {!!players?.length && (
                 <ul className="flex flex-wrap gap-x-4 gap-y-1">
-                    {players.map((item, index) => (
-                        <li key={index}>
-                            <p className="text-sm text-gray-500">{item.name} has {item.land}ha land.</p>
-                        </li>
-                    ))}
+                    
                 </ul>
             )}
         </>
